f37934512a818d367df211946be9a040b510b101
Maciej Mikołajek <maciejmikolajek@icloud.com>
2018-11-20 19:13:07 +0100

    Initial Commit

---
+++ Mole/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Mole
+//
+//  Created by Maciej Mikołajek on 20/11/2018.
+//  Copyright © 2018 Maciej Mikołajek. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Mole/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mole
+//
+//  Created by Maciej Mikołajek on 20/11/2018.
+//  Copyright © 2018 Maciej Mikołajek. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
